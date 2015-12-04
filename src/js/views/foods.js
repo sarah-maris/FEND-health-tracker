7971c80959191bbc635cb56a1a1461a50241ef27
Sarah Maris <sarah.87.maris@gmail.com>
2015-12-04 17:50:17 -0500

    Remove unneeded functions in food.js

--- src/js/views/foods.js
+++ src/js/views/foods.js
@@ -9,12 +9,9 @@
     //Template function for each food item
     template: _.template($('#item-template').html()),
 
-    //Bind functions to events needed for editing and removing food items
+    //Bind clear function to clicks on 'destroy' class items
     events: {
-      'dblclick label.view': 'edit',
       'click .destroy': 'clear',
-      'keypress .edit': 'updateOnEnter',
-      'blur .edit': 'close'
     },
 
     //Set up listeners for food items
@@ -26,47 +23,10 @@
     // Render the food item
     render: function () {
 
-      //get data from Firebase
+      //get data from Firebase and render using template
       this.$el.html( this.template( this.model.attributes ) );
-
-      //Add edit class to input element
-      this.$input = this.$('.edit');
       return this;
 
-    },
-
-    //Allow item to be edited
-    edit: function () {
-
-      //Change row class to 'editing' mode
-      this.$el.addClass('editing');
-      this.$input.focus();
-
-    },
-
-    // Close the `editing` mode and save changes to Firebase
-    close: function () {
-      var value = this.$input.val().trim();
-
-      //If item title exists, save it to Firebase
-      if ( value ) {
-        this.model.save({ title: value });
-
-      //If item is empty, delete it from Firebase
-      } else {
-        this.clear();
-      }
-
-      //Remove 'editing" class
-      this.$el.removeClass('editing');
-
-    },
-
-    //Fire the 'close' function on when hit 'enter' key
-    updateOnEnter: function( e ) {
-      if (e.which === ENTER_KEY ) {
-        this.close();
-      }
     },
 
     //Delete the item from Firebase and remove its view.
