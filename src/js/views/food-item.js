15c02a358dc62ec3ac68c0ed3d2445d770a30e86
Sarah Maris <sarah.87.maris@gmail.com>
2015-12-15 08:55:16 -0500

    Add success/error message in destroy for debugging

--- src/js/views/food-item.js
+++ src/js/views/food-item.js
@@ -15,13 +15,13 @@ app.FoodView = Backbone.View.extend({
 
   //Set up listeners for food items
   initialize: function () {
-    this.listenTo(this.model, 'change', this.render);
+   // this.listenTo(this.model, 'change', this.render);
     this.listenTo(this.model, 'destroy', this.remove);
   },
 
   // Render the food item
   render: function () {
-
+console.log("here in render food item")
     //get data from Firebase and render using template
     this.$el.html( this.itemTemplate( this.model.attributes ) );
 
@@ -32,7 +32,17 @@ app.FoodView = Backbone.View.extend({
   //Delete the item from Firebase and remove its view.
   clear: function () {
     console.log("here in clear")
-    this.model.destroy();
+    this.model.destroy(
+{
+   success : function(model) {
+       console.log("here in success")
+   },
+   error : function(
+    ) { console.log("here in error")},
+
+
+}
+      );
         console.log("after destroy")
   }
 });
