14d4b121b9b38e51a18666929208446016f820b3
Sarah Maris <sarah.87.maris@gmail.com>
2015-12-15 23:18:21 -0500

    Change removal function from model.destroy() to collection.remove()

--- src/js/views/food-item.js
+++ src/js/views/food-item.js
@@ -5,7 +5,7 @@ app.FoodView = Backbone.View.extend({
   //Element tag for food items
   tagName:  'tr',
 
-  //Template function for each food item
+  //Template to render each food item
   itemTemplate: _.template($('#item-template').html()),
 
   //Bind clear function to clicks on 'destroy' class items
@@ -15,34 +15,25 @@ app.FoodView = Backbone.View.extend({
 
   //Set up listeners for food items
   initialize: function () {
-   // this.listenTo(this.model, 'change', this.render);
+
     this.listenTo(this.model, 'destroy', this.remove);
   },
 
   // Render the food item
   render: function () {
-console.log("here in render food item")
-    //get data from Firebase and render using template
+
+    //Get data from Firebase and render using template
     this.$el.html( this.itemTemplate( this.model.attributes ) );
 
     return this;
 
   },
 
-  //Delete the item from Firebase and remove its view.
+  //Delete the item from Firebase and remove its view
   clear: function () {
-    console.log("here in clear")
-    this.model.destroy(
-{
-   success : function(model) {
-       console.log("here in success")
-   },
-   error : function(
-    ) { console.log("here in error")},
 
+    app.appView.foodList.remove(this.model);
 
-}
-      );
-        console.log("after destroy")
   }
+
 });
